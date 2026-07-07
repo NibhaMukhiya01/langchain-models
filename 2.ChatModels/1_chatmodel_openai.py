793ead1e4bc7307823c9cf0bb37cfc9a33e423ee
NibhaMukhiya01 <nibha0227086@gmail.com>
2026-07-08 04:16:45 +0530

    Update 1_chatmodel_openai.py

--- 2.ChatModels/1_chatmodel_openai.py
+++ 2.ChatModels/1_chatmodel_openai.py
@@ -1,10 +1,10 @@
-from langchain_openai import ChatOpenAI
+from langchain_openai import ChatOpenAI ##import chat model of open ai  ## use can see models at platform.openai.com
 from dotenv import load_dotenv
 
 load_dotenv()
 
-model = ChatOpenAI(model='gpt-4', temperature=1.5, max_completion_tokens=10)
+model = ChatOpenAI(model='gpt-4', temperature=1.5, max_completion_tokens=10) ##model is varial save object chatopenai
 
-result = model.invoke("Write a 5 line poem on cricket")
+result = model.invoke("Write a 5 line poem on cricket") ## invoke method
 
 print(result.content)
