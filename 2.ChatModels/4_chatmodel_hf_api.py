ef1964799558be694a9ea3b64aee1a3eabe42127
NibhaMukhiya01 <nibha0227086@gmail.com>
2026-07-10 21:23:50 +0530

    Enhance comments for Hugging Face API integration
    Updated comments for clarity on Hugging Face API usage.

--- 2.ChatModels/4_chatmodel_hf_api.py
+++ 2.ChatModels/4_chatmodel_hf_api.py
@@ -1,11 +1,11 @@
-from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint ##we have to import huggingfacepoint when we want to use hf apis
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv()##func call
 
-llm = HuggingFaceEndpoint(
-    repo_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
-    task="text-generation"
+llm = HuggingFaceEndpoint(  ##creating llm for hf by endpoint
+    repo_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0", ##repo id of tiny llama model api key
+    task="text-generation" ##tiny llama can do text generation
 )
 
 model = ChatHuggingFace(llm=llm)
